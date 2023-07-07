38d97a0ef7df7d4483b8e29b2f38894d7c0c7209
Jan Winkelmann (keks) <git@keks.cool>
2023-07-07 23:30:37 +0200

    impl Item for all uints (macro)

--- src/item/uint.rs
+++ src/item/uint.rs
@@ -1,21 +1,19 @@
-use super::Item;
+macro_rules! impl_Item_uint {
+    ($type:ty) => {
+        impl $crate::item::Item for $type {
+            fn zero() -> Self {
+                0
+            }
 
-impl Item for u32 {
-    fn zero() -> Self {
-        0
-    }
-
-    fn next(&self) -> Self {
-        self + 1
-    }
+            fn next(&self) -> Self {
+                self + 1
+            }
+        }
+    };
 }
 
-impl Item for u64 {
-    fn zero() -> Self {
-        0
-    }
-
-    fn next(&self) -> Self {
-        self + 1
-    }
-}
+impl_Item_uint!(u8);
+impl_Item_uint!(u16);
+impl_Item_uint!(u32);
+impl_Item_uint!(u64);
+impl_Item_uint!(u128);
